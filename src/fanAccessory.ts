b08e331a296e5969abde1f642720d11bebcb472f
Vivian <vchong@gmail.com>
2020-10-26 17:17:12 -0700

    update for fans with integrated lights

--- src/fanAccessory.ts
+++ src/fanAccessory.ts
@@ -5,6 +5,7 @@ import { Device } from '@nightbird/haiku-senseme';
 
 export class HaikuPlatformFanAccessory {
   private service: Service;
+  private lightService!: Service;
   private device: Device;
 
   // TODO: Get this into haiku-senseme.
@@ -44,6 +45,57 @@ export class HaikuPlatformFanAccessory {
     this.service = this.accessory.getService(this.platform.Service.Fanv2) ||
       this.accessory.addService(this.platform.Service.Fanv2, this.accessory.context.device.name);
 
+    this.device.device.hasLight.listen()
+      .on('change', has_light => {
+        this.platform.log.debug(`Got updated hasLight (${this.device.name}): ${has_light}`);
+        if (has_light) {
+          this.lightService = this.accessory.getService(this.platform.Service.Lightbulb) ||
+          this.accessory.addService(this.platform.Service.Lightbulb, this.accessory.context.device.name);
+
+          this.lightService.setCharacteristic(this.platform.Characteristic.Name, this.accessory.context.device.name);
+
+          // register handlers for the On/Off Characteristic
+          this.lightService.getCharacteristic(this.platform.Characteristic.On)
+            .on('set', this.setOn.bind(this))
+            .on('get', this.getOn.bind(this));
+
+          // register handlers for the Brightness Characteristic
+          this.lightService.getCharacteristic(this.platform.Characteristic.Brightness)
+            .on('set', this.setBrightness.bind(this))
+            .on('get', this.getBrightness.bind(this));
+
+          // ColorTemperature - min/max hardcoded because of refresh time
+          this.lightService.getCharacteristic(this.platform.Characteristic.ColorTemperature)
+            .on('set', this.setColorTemperature.bind(this))
+            .on('get', this.getColorTemperature.bind(this))
+            .setProps({ minValue: 200, maxValue: 454 });
+
+          // listen for changes to properties we care about
+          this.device.light.power.listen()
+            .on('change', power => {
+              this.platform.log.debug(`Got updated power value (${this.device.name}): ${power}`);
+              if (power === 'ON') {
+                this.lightService.updateCharacteristic(this.platform.Characteristic.On, true);
+              }
+              if (power === 'OFF') {
+                this.lightService.updateCharacteristic(this.platform.Characteristic.On, false);
+              }
+            });
+
+          this.device.light.brightness.listen()
+            .on('change', brightness => {
+              this.lightService.updateCharacteristic(this.platform.Characteristic.Brightness, brightness / 16 * 100);
+              this.platform.log.debug(`Got updated brightness (${this.device.name}): ${brightness}`); 
+            });
+
+          this.device.light.temperature.listen()
+            .on('change', temperature => {
+              this.lightService.updateCharacteristic(this.platform.Characteristic.ColorTemperature, 1000000 / temperature);
+              this.platform.log.debug(`Got updated color temp (${this.device.name}): ${temperature}`);
+            });
+            
+        }
+      });
     // To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
     // when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
     //this.accessory.getService('NAME') ?? this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE');
@@ -134,6 +186,94 @@ export class HaikuPlatformFanAccessory {
     this.platform.log.debug(`(${this.device.name}) API speed ${speed} Homekit speed (${currentSpeed})`);
 
     callback(null, currentSpeed);
+  }
+
+
+  /**
+   * SET On
+   */
+  setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    this.platform.log.debug(`Set Characteristic On (${this.device.name}) ->`, value);
+    if (value) {
+      this.device.light.power.value = 'on';
+    } else {
+      this.device.light.power.value = 'off';
+    }
+    callback(null);
+  }
+
+  /**
+   * GET On
+   */
+  getOn(callback: CharacteristicGetCallback) {
+    let currentOn = this.lightService.getCharacteristic(this.platform.Characteristic.On).value as boolean;
+    const power = this.device.light.power.value;
+    this.platform.log.debug(`Current power (${this.device.name}): ${power}`);
+    if (power === 'on') {
+      currentOn = true;
+    }
+    if (power === 'off') {
+      currentOn = false;
+    }
+    callback(null, currentOn);
+  }
+
+  /**
+   * SET Brightness
+   */
+  setBrightness(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    this.platform.log.debug(`Set Characteristic Brightness (${this.device.name}) ->`, value);
+    this.platform.log.debug('maximum from API', this.device.light.brightness.maximum.value);
+    const maxVal = this.device.light.brightness.maximum.value || 16;
+    const apiValue = value as number / 100 * maxVal;
+    this.platform.log.debug(`Set Characteristic Brightness (apiValue) (${this.device.name}) ->`, apiValue);
+    this.device.light.brightness.value = apiValue;
+
+    callback(null);
+  }
+
+  /**
+   * GET Brightness
+   */
+  getBrightness(callback: CharacteristicGetCallback) {
+    //const maxVal = this.device.light.brightness.maximum.value || 16;
+    // hardcoded for now, how to make sure that the values are refreshed?
+    const maxVal = 16;
+    let currentBrightness = this.lightService.getCharacteristic(this.platform.Characteristic.Brightness).value as number;
+    const brightness = this.device.light.brightness.value;
+    
+    currentBrightness = brightness / maxVal * 100;
+    this.platform.log.debug(`(${this.device.name}) API brightness: ${brightness} Homekit brightness: ${currentBrightness}`);
+
+    callback(null, currentBrightness);
+  }
+
+  /**
+   * SET ColorTemperature
+   */
+  setColorTemperature(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    this.platform.log.debug(`Set Characteristic ColorTemperature (${this.device.name}) -> `, value);
+    const tempValue = value as number;
+    //this.platform.log.debug('maximum from API', this.device.light.temperature.maximum.value);
+    //const maxVal = this.device.light.temperature.maximum.value || 5000;
+    const apiValue = 1000000 / tempValue;
+    this.platform.log.debug(`Set Characteristic ColorTemperature (${this.device.name}) (apiValue) -> `, apiValue);
+    this.device.light.temperature.value = apiValue;
+
+    callback(null);
+  }
+
+  /**
+   * GET ColorTemperature
+   */
+  getColorTemperature(callback: CharacteristicGetCallback) {
+    let currentTemperature = this.lightService.getCharacteristic(this.platform.Characteristic.ColorTemperature).value as number;
+    const temperature = this.device.light.temperature.value;
+
+    currentTemperature = 1000000 / temperature;
+    this.platform.log.debug(`(${this.device.name}) API colortemp: ${temperature} Homekit colortemp: ${currentTemperature}`);
+
+    callback(null, currentTemperature);
   }
 
 }
